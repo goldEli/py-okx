8ff7714abbf99f545daf5920ce27bf0b0d5b2fa6
eli <eli107377@weexdev.com>
2025-03-17 20:31:29 +0800

    feat: modify sz

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ from lib.config import get_email_info
 import lib.market as market
 from lib.order import place_market_order
 from lib.acount import get_account_balance
-symbol = "ETH-USDT-SWAP"
+# symbol = "ETH-USDT-SWAP"
 # symbol = "BTC-USDT-SWAP"
-# symbol = "TRUMP-USDT-SWAP"
+symbol = "TRUMP-USDT-SWAP"
 # symbol = "SOL-USDT-SWAP"
 current_price = market.get_current_price(symbol)
 print(current_price)
 
 place_market_order(symbol, "short", current_price['last_price'])
 # place_market_order(symbol, "long", current_price['last_price'])
-get_account_balance()
+# get_account_balance()
 
