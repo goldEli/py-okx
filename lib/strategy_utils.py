746c83cdbaab30efb1346f18f597ac02730d3cda
eli <eli107377@weexdev.com>
2025-03-22 12:10:37 +0800

    feat: modify strategy

--- lib/strategy_utils.py
+++ lib/strategy_utils.py
@@ -37,6 +37,8 @@ def is_top_upper_strategy(data):
     high_1d = float(data['1d_high'])
     h1, h2, candle_length, upper_shadow_length, lower_shadow_length = get_candle_info(data)
 
+    # 上引线大于下影线x倍数
+    is_upper_greater_than_lower = upper_shadow_length > lower_shadow_length * 5
     # 是否有上引线
     is_upper = (upper_shadow_length / high) > top_multiple
     # 是否是顶
@@ -44,9 +46,10 @@ def is_top_upper_strategy(data):
     # 是否是针(上影线是蜡烛的50%)
     is_needle = candle_length > 0 and upper_shadow_length / candle_length > 1
 
-    strategy1 = is_upper and is_top and is_needle
+
+    strategy1 = is_upper and is_top and is_needle  and is_upper_greater_than_lower
         
-    strategy2 = is_top and candle_length > 0 and upper_shadow_length / candle_length > 2
+    strategy2 = is_top and candle_length > 0 and upper_shadow_length / candle_length > 2 and is_upper_greater_than_lower
 
     return strategy1 or strategy2
 
@@ -54,13 +57,17 @@ def is_top_upper_strategy(data):
 def is_normal_upper_strategy(data):
     high = float(data['high'])
     h1, h2, candle_length, upper_shadow_length, lower_shadow_length = get_candle_info(data)
-    return (upper_shadow_length / high) > multiple
+    # 上引线大于下影线x倍数
+    is_upper_greater_than_lower = upper_shadow_length > lower_shadow_length * 5
+    return (upper_shadow_length / high) > multiple and is_upper_greater_than_lower
 
 # 是否是冲底下影线策略
 def is_bottom_lower_strategy(data):
     low = float(data['low'])
     low_1d = float(data['1d_low'])
     h1, h2, candle_length, upper_shadow_length, lower_shadow_length = get_candle_info(data)
+    # 下引线大于上影线x倍数
+    is_lower_greater_than_upper = lower_shadow_length > upper_shadow_length * 5
     # 是否有下引线
     is_lower = lower_shadow_length/(lower_shadow_length + h2) > top_multiple
     # 是否是底
@@ -68,9 +75,9 @@ def is_bottom_lower_strategy(data):
     # 是否是针(下影线是蜡烛的50%)
     is_needle = candle_length > 0 and lower_shadow_length / candle_length > 1
 
-    strategy1 = is_lower and is_bottom and is_needle
+    strategy1 = is_lower and is_bottom and is_needle and is_lower_greater_than_upper
 
-    strategy2 = is_bottom and candle_length > 0 and lower_shadow_length / candle_length > 2
+    strategy2 = is_bottom and candle_length > 0 and lower_shadow_length / candle_length > 2 and is_lower_greater_than_upper
 
     return strategy1 or strategy2
 
@@ -78,4 +85,6 @@ def is_bottom_lower_strategy(data):
 def is_normal_lower_strategy(data):
     low = float(data['low'])
     h1, h2, candle_length, upper_shadow_length, lower_shadow_length = get_candle_info(data)
-    return lower_shadow_length/(lower_shadow_length + h2) > multiple
+    # 下引线大于上影线x倍数
+    is_lower_greater_than_upper = lower_shadow_length > upper_shadow_length * 5
+    return lower_shadow_length/(lower_shadow_length + h2) > multiple and is_lower_greater_than_upper
