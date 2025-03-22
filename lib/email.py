15e127d47bacd5ba3ad2ab685ccf7376d37f0695
eli <eli107377@weexdev.com>
2025-03-22 10:55:32 +0800

    feat: modify msg

--- lib/email.py
+++ lib/email.py
@@ -66,7 +66,7 @@ def send_email_for_trigger(data, direction):
 
     sideText = "多" if direction == "long" else "空"
     subject = f"告警提示"
-    body = f"官式针法触发开{sideText}\n\nsymbol：{symbol}\nprice：{current_price}\nhigh：{high}\nlow：{low}\nopen：{open}\nclose：{close}\nside：{direction}\n当天最高价：{high_1d}\n当天最低价：{low_1d}\n发生时间：{datetime.fromtimestamp(int(data['timestamp']) / 1000).strftime('%Y-%m-%d %H:%M:%S')}"
+    body = f"官式针法触发开{sideText}\n版本：1.0.0\nsymbol：{symbol}\nprice：{current_price}\nhigh：{high}\nlow：{low}\nopen：{open}\nclose：{close}\nside：{direction}\n当天最高价：{high_1d}\n当天最低价：{low_1d}\n发生时间：{datetime.fromtimestamp(int(data['timestamp']) / 1000).strftime('%Y-%m-%d %H:%M:%S')}"
     # 邮件发送使用单独的线程, 防止阻塞主进程. 
     threading.Thread(target=send_email, args=(subject, body)).start()
 
